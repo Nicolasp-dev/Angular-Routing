149cf000be257d826cb2eb50ffec2ea684114041
nicolaspc-dev <nicolaspc.wd@gmail.com>
2023-05-14 20:57:24 -0500

    Protecting routes with Guards

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,28 @@
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs-compat';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.isAuth().then((auth: boolean) => {
+      if (auth) {
+        return true;
+      } else {
+        this.router.navigate(['/']);
+        return false;
+      }
+    });
+  }
+}
